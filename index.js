552ad4c5440f7c6f657357aa6e119bb658bfabdf
ezebarredo <ezequielbarredo@gmail.com>
2024-03-28 18:01:26 +0100

    create new card and generate id with nanoid with app.post

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const cards = require("./data/cards.js");
 const express = require("express");
 const cors = require("cors");
+const id = require("nanoid");
 const app = express();
 const PORT = 4000;
 const bodyParser = require("body-parser");
-
 // middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -31,19 +31,43 @@ app.get("/api/cards/:cardId", (request, response) => {
     : response.status(404).json({ msg: "id not found" });
 });
 
+// app.post("/api/cards", (request, response) => {
+//   const {
+//     body: { front, back, image },
+//   } = request;
+//   response.status(201).json({ front });
+// });
+
+// TODO: create a new card with front, back, image and nanoid and generate id with it and push new card to cards.
+// read about: delete and patch and http codes.
+// how to do fetch with post and json.
+// fetch syntax
+
+// fetch('https://example.com/api/data', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     key1: 'value1',
+//     key2: 'value2'
+//   })
+// })
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error(error));
+
+// 201 created
 app.post("/api/cards", (request, response) => {
   const {
     body: { front, back, image },
   } = request;
-  console.log(front);
-  response.status(201).json({ front });
+  const newCard = { id, front, back, image };
+  const cardWithNewCard = cards.push(newCard);
+  response.status(201).json({ cards });
 });
 
-// TODO: create a new card with front, back and image and also nanoid and generate id with it and push new card to cards.
-// read about: delete and patch and http codes.
-// how to do fetch with post and json.
-
-// Next lesson delete and patch.
+// TODO: Next lesson delete and patch.
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
