2585415d1bf5b31dcd24089507b80aaf065421ba
ezebarredo <ezequielbarredo@gmail.com>
2024-04-01 11:57:56 +0200

    Homework in progress: Validation for params | update DELETE and PATCH with validation | unified card.id === cardId function and convert it to middleware function

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const cards = require("./data/cards.js");
+// IMPORTS
 const express = require("express");
 const cors = require("cors");
-const id = require("nanoid");
+const nanoid = require("nanoid").nanoid;
 const app = express();
 const PORT = 4000;
 const bodyParser = require("body-parser");
-// middleware
+
+// DATA
+let cards = require("./data/cards.js");
+
+const cardIdMiddleware = function (request, response, next) {
+  const cardId = request.params.cardId;
+  request.cardIdMiddleware = (card) => card.id === cardId;
+  next();
+};
+
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cardIdMiddleware);
 
 // API 1
 app.get("/hello", (request, response, next) => {
@@ -16,21 +27,24 @@ app.get("/hello", (request, response, next) => {
 
 // API 2
 // TODO: 1. Copy file cards.js from Next.js app into server project and import here.
-//
+//  GET method requests a representation of the specified resource. Requests using GET should only retrieve data.
 app.get("/api/cards", (request, response, next) => {
   response.status(200).json({ cards });
 });
 
 // API 3
 // TODO: 2. Make another API: /api/cards/:cardId -- SINGLE CARD
+//
 app.get("/api/cards/:cardId", (request, response) => {
-  const cardId = request.params.cardId;
-  const getCardId = cards.find((card) => card.id === cardId);
+  // const cardId = request.params.cardId;
+  // const getCardId = cards.find((card) => card.id === cardId);
+  const getCardId = cards.find(request.cardIdMiddleware);
   getCardId
     ? response.status(200).json(getCardId)
     : response.status(404).json({ msg: "id not found" });
 });
 
+// Example POST
 // app.post("/api/cards", (request, response) => {
 //   const {
 //     body: { front, back, image },
@@ -57,18 +71,55 @@ app.get("/api/cards/:cardId", (request, response) => {
 // .then(data => console.log(data))
 // .catch(error => console.error(error));
 
-// 201 created
+// 201 created | POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.
 app.post("/api/cards", (request, response) => {
-  const {
-    body: { front, back, image },
-  } = request;
-  const newCard = { id, front, back, image };
-  const cardWithNewCard = cards.push(newCard);
-  response.status(201).json({ cards });
+  const { body } = request;
+  const newCard = { ...body, id: nanoid() };
+  cards.push(newCard);
+  response.status(201).json({ newCard });
 });
 
-// TODO: Next lesson delete and patch.
+// TODO: Next lesson patch and delete.
+
+// PATCH method applies partial modifications to a resource.
+app.patch("/api/cards/:cardId", (request, response) => {
+  // const {params: {cardId}, body} = request;
+  const {
+    params: { cardId },
+    body,
+  } = request;
+  if (body.front || body.back || body.image) {
+    cards = cards.map((card) =>
+      card.id === cardId
+        ? {
+            ...card,
+            ...body,
+          }
+        : card
+    );
+    response.status(200).json({ cards });
+  } else {
+    response.status(400).json({ msg: "Please enter the correct data" });
+  }
+});
+
+// DELETE
+app.delete("/api/cards/:cardId", (request, response) => {
+  const {
+    params: { cardId },
+  } = request;
+  if (cards.find((card) => card.id === cardId)) {
+    cards = cards.filter((card) => card.id !== cardId);
+    response.status(200).json({ msg: "Card deleted successfully" });
+  } else {
+    response.status(200).json({ msg: "Please enter the correct data" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+// HOMEWORK 29th March
+// Validation for params | update DELETE and PATCH with validation | unified card.id === cardId function and convert it to middleware function
+// check PUT and PATCH (check difference) and write additional endpoint
